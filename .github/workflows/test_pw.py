734a9effaddecfa142a7730a7731bcef0f6563f1
k6n5s7j8n5-svg <k6n5s7j8n5@gmail.com>
2026-02-24 12:10:10 +0900

    Delete .github directory

--- .github/workflows/test_pw.py
+++
@@ -1,8 +0,0 @@
-from playwright.sync_api import sync_playwright
-
-with sync_playwright() as p:
-    browser = p.chromium.launch(headless=True, args=["--no-sandbox"])
-    page = browser.new_page()
-    page.goto("https://example.com")
-    print("TITLE:", page.title())
-    browser.close()
